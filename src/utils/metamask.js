babf399480d9430a353b7c7a93ab787f5adcf888
sheng <qzwxsa1234@gmail.com>
2021-12-17 00:38:26 +0800

    Feat: handle no extension installed

--- src/utils/metamask.js
+++ src/utils/metamask.js
@@ -1,33 +1,29 @@
 import detectEthereumProvider from '@metamask/detect-provider'
 
-const isExtensionIntstalled = async () => {
-  const provider = await detectEthereumProvider()
-
-  if (provider) {
-    return true
-  } else {
-    return false
-  }
-}
-
 export const getSelectedAddress = async () => {
-  const provider = await detectEthereumProvider()
-  return isExtensionIntstalled() ? await provider.selectedAddress : null
+  try {
+    const provider = await detectEthereumProvider()
+    return provider ? await provider.selectedAddress : null
+  } catch (error) {
+    return null
+  }
 }
 
 export const connect = async () => {
   const provider = await detectEthereumProvider()
 
-  if (!isExtensionIntstalled()) return alert('Please install MetaMask!')
-
   // await provider.request({
   //   method: 'wallet_requestPermissions',
   //   params: [{ eth_accounts: {} }],
   // })
-  await provider.request({
-    method: 'eth_requestAccounts',
-    params: [{ eth_accounts: {} }]
-  })
-
-  return provider
+  if (provider) {
+    await provider.request({
+      method: 'eth_requestAccounts',
+      params: [{ eth_accounts: {} }]
+    })
+    return provider
+  } else {
+    alert('Please install MetaMask!')
+    throw new Error('No provider found')
+  }
 }
