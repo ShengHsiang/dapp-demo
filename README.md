7734cf5d0f427c54af82bf717ea4ce7b0de7ac62
sheng <qzwxsa1234@gmail.com>
2021-12-17 00:04:16 +0800

    Feat: metamask utils

--- README.md
+++ README.md
@@ -1,29 +1,14 @@
 # Dapp-demo
 
-## Project setup
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Run your unit tests
-```
-yarn test:unit
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## 概覽
+使用的技術： vue, vuex, tailwinds, web3.js 搭配 metamask 錢包
+功能規劃：
+1. 檢測是否安裝. Metamask 插件（已完成）
+2. 連結 Metamask 錢包（已完成）
+3. 查詢帳戶餘額（已完成）
+4. 查詢號戶地址（已完成）
+5. 連結 NFT 合約（已完成）
+6. 展示 NFT
+7. 調用智能合約購買 NFT （已完成）-> 此合約的 NFT 已經銷售完畢
+8. 購買失敗處理及提示
+9. 各類型異常處理以及提示
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,71 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Web3 from 'web3'
+import { connect, getSelectedAddress } from '@/utils/metamask'
+import { getContract } from '@/utils/getContract'
+import { address, ABI } from '@/utils/constants/nftContract'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+export const store = new Vuex.Store({
+  strict: true,
   state: {
+    selectedAddress: null,
+    balance: null,
+    contractInstance: null
+  },
+  getters: {
+    getSelectedAddress: state => {
+      return state.selectedAddress
+    }
   },
   mutations: {
+    setSelectedAddress (state, selectedAddress) {
+      state.selectedAddress = selectedAddress
+    },
+    setBalance (state, balance) {
+      state.balance = balance
+    },
+    registerContractInstance (state, contractInstance) {
+      state.contractInstance = contractInstance
+    }
   },
   actions: {
-  },
-  modules: {
+    async initialize ({ dispatch, commit }) {
+      try {
+        const selectedAddress = await dispatch('setSelectedAddress')
+        if (selectedAddress) {
+          dispatch('initialWallet')
+          dispatch('getContractInstance')
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    async connectToMetamask ({ dispatch, commit }) {
+      try {
+        await connect()
+        await dispatch('setSelectedAddress')
+        await dispatch('initialWallet')
+        await dispatch('getContractInstance')
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async setSelectedAddress ({ commit }) {
+      const selectedAddress = await getSelectedAddress()
+      commit('setSelectedAddress', selectedAddress)
+      return selectedAddress
+    },
+    async initialWallet ({ commit, state }) {
+      const web3 = await new Web3(window.ethereum)
+      const balance = await web3.eth.getBalance(state.selectedAddress)
+      const balanceInEther = await web3.utils.fromWei(balance, 'ether')
+      commit('setBalance', balanceInEther)
+    },
+    getContractInstance ({ commit }) {
+      const contractfunc = () => getContract(address, ABI)
+      commit('registerContractInstance', contractfunc)
+    }
   }
 })
